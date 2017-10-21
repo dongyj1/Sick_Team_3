8c3b67b52cd8e0c7106e7309b8f06b5847a98391
TouyamaNao <dongyanj163@163.com>
2017-10-21 12:44:55 -0400

    add data tag

--- extractDataFromXML.py
+++ extractDataFromXML.py
@@ -1,5 +1,21 @@
-import xml.etree.ElementTree as etree
-data = etree.parse('production_xmldata.2017-07-04.xml')
-for a in list(data.getroot()):
-	# if (a.tag == 'objectdata'):
-	print(a)
+import xml.etree.ElementTree as ET
+import fileinput
+
+
+filename = 'production_xmldata.2017-07-04.xml'
+
+data = ''
+with open(filename, 'r') as f:
+    data = f.read().replace('\n', '')
+if (not data.startswith('<data>')):
+    data = '<data>'+data+'</data>'
+
+dictionary = {}
+root = ET.fromstring(data)
+# print(data)
+print(root)
+for child in root:
+    # dictionary[child.tag] = child.attrib
+    if(child.tag=='objectdata'):
+
+
