de70c9d6d81ae28648476dc16c3ddc1b8bfe854c
DYJ <dyj@dhcp-wifi-8021x-155-41-0-21.bu.edu>
2017-12-09 11:05:37 -0500

    add epoch test

--- LSTM_modelF_withTimeInterval.py
+++ LSTM_modelF_withTimeInterval.py
@@ -19,6 +19,7 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
 import keras.backend as K
+import datetime
 
 def matthews_correlation(y_true, y_pred):
     """Matthews correlation metric.
@@ -191,25 +192,30 @@ test_y = test[:, 30]
 train_X = train_X.reshape((train_X.shape[0], 1, train_X.shape[1]))
 test_X = test_X.reshape((test_X.shape[0], 1, test_X.shape[1]))
 print(train_X.shape, train_y.shape, test_X.shape, test_y.shape)
- 
+
+# test for parameters
+epoches = [2, 3, 4, 5]
+for epoch in epoches:
 # design network
-model = Sequential()
-model.add(LSTM(50, input_shape=(train_X.shape[1], train_X.shape[2])))
-model.add(Dense(1))
-model.compile(loss='binary_crossentropy', optimizer='adam',metrics=[precision, recall, fmeasure])
-# fit network
-history = model.fit(train_X, train_y, epochs=50, batch_size=72, validation_data=(test_X, test_y), verbose=2, shuffle=False)
-# plot history
-pyplot.plot(history.history['loss'], label='train_loss')
-pyplot.plot(history.history['val_loss'], label='test_loss')
-pyplot.plot(history.history['fmeasure'], label='train_f1')
-pyplot.plot(history.history['val_fmeasure'], label='test_f1')
-pyplot.plot(history.history['precision'], label='train_p')
-pyplot.plot(history.history['val_precision'], label='test_p')
-pyplot.plot(history.history['recall'], label='train_r')
-pyplot.plot(history.history['val_recall'], label='test_r')
-pyplot.legend()
-pyplot.show()
+    model = Sequential()
+    model.add(LSTM(50, input_shape=(train_X.shape[1], train_X.shape[2])))
+    model.add(Dense(1))
+    model.compile(loss='binary_crossentropy', optimizer='adam',metrics=[precision, recall, fmeasure])
+    # fit network
+    history = model.fit(train_X, train_y, epochs=epoch, batch_size=72, validation_data=(test_X, test_y), verbose=2, shuffle=False)
+    # plot history
+    pyplot.plot(history.history['loss'], label='train_loss')
+    pyplot.plot(history.history['val_loss'], label='test_loss')
+    pyplot.plot(history.history['fmeasure'], label='train_f1')
+    pyplot.plot(history.history['val_fmeasure'], label='test_f1')
+    pyplot.plot(history.history['precision'], label='train_p')
+    pyplot.plot(history.history['val_precision'], label='test_p')
+    pyplot.plot(history.history['recall'], label='train_r')
+    pyplot.plot(history.history['val_recall'], label='test_r')
+    pyplot.legend()
+    print('saving plots ...')
+    pyplot.savefig('runningPlots'+str(datetime.datetime.now())+'.png', bbox_inches="tight")
+# pyplot.show()
 ''' 
 # make a prediction
 yhat = model.predict(test_X)
